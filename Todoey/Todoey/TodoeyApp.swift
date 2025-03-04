bbd5c975daae84e2df45b615604c7e0ca6465397
MatthewBarghout <mattbarg@unc.edu>
2025-03-04 15:06:19 -0500

    Todoey 1.0

---
+++ Todoey/Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Matthew Barghout on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
